1a715343a2f3e50142fb7d5d6341c08942c13809
Amos Ombewa <aombewa@csquarednet.com>
2022-06-08 10:37:08 +0300

    Updated formatting in script

--- form/customers-script.js
+++ form/customers-script.js
@@ -24,15 +24,20 @@ $(document).ready(function () {
 
 
     function createCustomerCard(customer) {
+
         const subtitle = $("<p></p>").text(customer.email);
         subtitle.addClass("card-subtitle")
+
         const title = $("<h4></h4>").text(customer.name);
         title.addClass("card-title")
+
         const header = $("<div></div>");
         header.addClass("card-header");
         header.append(title,subtitle);
+
         const card = $("<div></div>").append(header);
         card.addClass("card col-lg-2 col-md-4 col-sm-6")
+        
         $("body").append(card);
     }
 
